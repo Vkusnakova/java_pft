54e1644a96b2323eea65e6977c7be2e9940ce543
Daria Kozhevnikova <kozhevnikova.d.n@gmail.com>
2018-01-02 18:17:13 +0300

    change module

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,12 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-  public static void main(String[] args) {
-     System.out.println("Hello, world!");
- }
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+    }
 }
+
