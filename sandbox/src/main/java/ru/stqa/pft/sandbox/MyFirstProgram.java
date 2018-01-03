38efe717dd6cb6cc6096f49eff2bbe9af6b6b975
Daria Kozhevnikova <kozhevnikova.d.n@gmail.com>
2018-01-04 00:25:15 +0300

    созданы новые классы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,24 +6,27 @@ public class MyFirstProgram {
         hello("world");
         hello("cat");
 
-        double len=5;
 
-        System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+        Square s = new Square(5);
 
-        double a=4;
-        double b=2;
-        System.out.println("Площадь прямоугольника со стороной " + a + " и " + b + " = " + area(a,b));
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+
+        Rectangle r = new Rectangle(4,6);
+
+        System.out.println("Площадь прямоугольника со стороной " + r.a + " и " + r.b + " = " + area(r));
     }
 
     public static void hello(String somebody) {
 
         System.out.println("Hello, " + somebody + "!");
     }
-    public static double area(double l) {
-        return l*l;
+
+    public static double area(Square s) {
+        return s.l * s.l;
     }
-    public static double area(double a, double b) {
-        return a*b;
+
+    public static double area(Rectangle r) {
+        return r.a * r.b;
     }
 
 }
