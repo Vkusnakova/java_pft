d0f6dea75e900c4dc0f43e00add37b04e868d470
Kozhevnikova, Daria <daria.kozhevnikova@emc.com>
2018-04-21 20:49:13 +0300

    add all browsers gor testing

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,7 +2,9 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import sun.plugin2.util.BrowserType;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
@@ -13,11 +15,26 @@ public class ApplicationManager {
     private GroupHelper groupHelper;
     public StringBuffer verificationErrors = new StringBuffer();
     public String baseUrl;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
 
     public void init() {
-        System.setProperty("webdriver.chrome.driver", "C://Users/kozhed/htmlui/chromedriver.exe");
-        driver = new ChromeDriver();
+        if (browser == org.openqa.selenium.remote.BrowserType.GOOGLECHROME) {
+            System.setProperty("webdriver.chrome.driver", "C://Users/kozhed/Documents/GitHub/java_pft/addressbook-web-tests/chromedriver.exe");
+            driver = new ChromeDriver();
+        } else if (browser == org.openqa.selenium.remote.BrowserType.FIREFOX) {
+            System.setProperty("webdriver.gecko.driver", "C://Users/Documents/GitHub/java_pft/addressbook-web-tests/geckodriver.exe");
+            driver = new FirefoxDriver();
+        }
+         else if (browser == org.openqa.selenium.remote.BrowserType.IE) {
+            System.setProperty("webdriver.ie.driver", "C://Users/kozhed/Documents/GitHub/java_pft/addressbook-web-tests/IEDriverServer.exe");
+                driver = new InternetExplorerDriver();
+        }
+
         baseUrl = "https://www.katalon.com/";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         groupHelper = new GroupHelper(driver);
